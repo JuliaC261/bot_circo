8a1e3834c4a2d4bbec6e45264a334ac4300e9090
J.C <99402892+JuliaC261@users.noreply.github.com>
2025-04-20 21:56:28 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def alma(ctx):
                 "\n"
                 "⠀⠀⠀⠀**◎** *Sua Alma é uma **__Recém - Chegada__***!*\n"
                 "\n"
-                "> -# *São aqueles que já acabaram de morrer e se despertaram nesse circo sem entender o que está acontecendo, [Saiba mais…](https://discordapp.com/channels/1352404572114653244/1354250241322516550)*"
+                "> -# - *São aqueles que já acabaram de morrer e se despertaram nesse circo sem entender o que está acontecendo, [Saiba mais…](https://discordapp.com/channels/1352404572114653244/1354250241322516550)*"
             ),
             "imagem": "https://media.discordapp.net/attachments/1149801910895910912/1362921665071288420/729_Sem_Titulo_20250325150216.jpg"
         },
@@ -42,7 +42,7 @@ async def alma(ctx):
                 "\n"
                 "⠀⠀⠀⠀**◎** *Sua Alma é uma **__Experiente__***!*\n"
                 "\n"
-                "> -# *São aqueles que já passaram tempo suficiente no circo para compreender suas regras e horrores, [Saiba mais…](https://discordapp.com/channels/1352404572114653244/1354250241322516550)*"
+                "> -# - *São aqueles que já passaram tempo suficiente no circo para compreender suas regras e horrores, [Saiba mais…](https://discordapp.com/channels/1352404572114653244/1354250241322516550)*"
             ),
             "imagem": "https://media.discordapp.net/attachments/1149801910895910912/1362921455716663417/729_Sem_Titulo_20250325170905.jpg?ex=68042704&is=6802d584&hm=5277f83cb25273eb00b9fb86fc8c9d84ebc0662cf91fe959b1ef22a36737caf2&=&format=webp&width=1860&height=620"
         },
@@ -57,7 +57,7 @@ async def alma(ctx):
                 "\n"
                 "⠀⠀⠀⠀**◎** *Sua Alma é uma **__Corrompida__***!*\n"
                 "\n"
-                "> -# *Foram tomadas pelo medo, ganância ou desespero, [Saiba mais…](https://discordapp.com/channels/1352404572114653244/1354250241322516550)*"
+                "> -# - *Foram tomadas pelo medo, ganância ou desespero, [Saiba mais…](https://discordapp.com/channels/1352404572114653244/1354250241322516550)*"
             ),
             "imagem": "https://media.discordapp.net/attachments/1149801910895910912/1362922657175175219/729_Sem_Titulo3_20250418194750.jpg"
         },
@@ -117,7 +117,7 @@ async def função(ctx):
                 "\n"
                 f"⠀⠀⠀⠀**◎** *Sua Função é **__{funcao_escolhida}__***!*\n"
                 "\n"
-        "> -# ****[Saiba mais…](https://discord.com/channels/1352404572114653244/1353079696325349478)****"
+        "> -# - ***[Saiba mais…](https://discord.com/channels/1352404572114653244/1353079696325349478)***"
     )
 
     imagem = "https://media.discordapp.net/attachments/1149801910895910912/1363672072471056475/A4H1Wk1U4BCeAAAAAElFTkSuQmCC.png"
